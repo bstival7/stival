1b7211493ba84c0393a64dc13c57345f65d0d508
bstivalASC4 <bernardostival@yahoo.com>
2017-07-24 15:23:07 -0400

    A lot of stuff

---
+++ stival-spaceinvaders/sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+    createCanvas(600,400);
+}
+
+function draw(){
+    background('black');
+
+}
+
+
+function Ship(){
+    this.x = width/2;
+    this.show = function (){
+        rect
+    }
+}
